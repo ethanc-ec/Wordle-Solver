af7601a472915c4eabbcfdb0c3958532f7278da0
weyliu <wesleyliu728@gmail.com>
2022-02-06 22:02:34 -0800

    made guess finder better
    added greyedorange functionality, which eliminated possible slots where oranges have been found as being possible slots for those letters

--- Solver.py
+++ Solver.py
@@ -7,9 +7,13 @@ def checkgreens(green, word):
         if val and word[idx] != val:
             return False
     return True
+def checkgreyoranged(greyoranges, word):
+    for idx, val in enumerate(greyoranges):
+        if word[idx] in val:
+            return False
+    return True
 
-
-def parsewords(green, orange, grey, words):
+def parsewords(green, orange, grey, greyedoranges, words):
     """Takes in 4 parameters:
     green a list containing all green characters and None as filler \norange a set of characters marked orange\ngrey a set of characters marked grey\nand words a set of words to parse through
     returns a list of newly parsed words meeting the parameters
@@ -23,25 +27,58 @@ def parsewords(green, orange, grey, words):
         for char in green:
             our_map[char] -= 1
 
-        if checkgreens(green, word) and orange.issubset(charset := set(list(word))) and grey.isdisjoint(set([x for x in our_map.keys() if our_map[x] > 0])):
+        if checkgreens(green, word) and orange.issubset(charset := set(list(word))) and grey.isdisjoint(set([x for x in our_map.keys() if our_map[x] > 0])) and checkgreyoranged(greyedoranges, word):
             newlist.add(word)
     return newlist
 
 
-def parseguesses(green, orange, grey, words):
+
+
+def parseguesses(orange, grey, greyedoranges, words):
     newlist = set()
     yesorange = set()
     for word in words:
         word = word[:5]
-        if orange.isdisjoint(charset := set(list(word))) and grey.isdisjoint(charset):
+        if orange.issubset(charset := set(list(word))) and grey.isdisjoint(charset) and checkgreyoranged(greyedoranges, word):
             newlist.add(word)
         elif grey.isdisjoint(set(list(word))):
             yesorange.add(word)
-    if len(newlist) == 0:
-        return yesorange
-    else:
-        return newlist
+    return newlist
 
+    
+def bestguess(possible_words, guess_list):
+    """
+    Takes in a list of words, finds the best word based on sum of occurrences of each letter
+    Needs work, weights for various parts
+    """
+    if len(guess_list) == 1:
+        return set([guess_list])
+    charhashmap = defaultdict(lambda:0)
+    for word in possible_words:
+        for char in word:
+            charhashmap[char] += 1
+    curmax = 0
+    dupemax = 0
+    maxval = set()
+    maxwithdupes = set()
+    for word in guess_list:
+        word = word[:5]
+        summ = 0
+        for char in word:
+            summ += charhashmap[char]
+        if len(set(list(word))) < len(word):
+            if summ > dupemax:
+                dupemax = summ
+                maxwithdupes = set([word])
+            elif summ == dupemax:
+                maxwithdupes.add(word)
+        else:
+            if summ > curmax:
+                curmax = summ
+                maxval = set([word])
+            elif summ == curmax:
+                maxval.add(word)
+    return maxval.union(maxwithdupes)
 
 def bestword(words):
     """
@@ -86,8 +123,10 @@ def test_run(words):
     """
     _greens = [None, None, None, None, None]
     _oranges = set()
+    _greyoranges = [set(),set(),set(),set(),set()]
     _grey = set()
     possiblities = words
+    guesslist = words
     while True:
         print("Green letters:", _greens)
         if len(_oranges) == 0:
@@ -95,28 +134,34 @@ def test_run(words):
         else:
             print("Orange letters:", _oranges)  
         print("Grey letters:", _grey)
-        possiblities = parsewords(_greens,_oranges,_grey, possiblities)
+        possiblities = parsewords(_greens,_oranges,_grey, _greyoranges, possiblities)
         print("Possible words:")
         print(possiblities)
-        w = bestword(parseguesses(_greens,_oranges,_grey, words))
         if len(possiblities) == 1:
             print("solve found: " + list(possiblities)[0])
             break
+        guesslist = parseguesses(_oranges,_grey, _greyoranges, words)
+        w = bestguess(possiblities, guesslist)
         print("Best words using current params:")
         print(w)
         guess = input("Type your guess:\n")
-        green = greeninput()
+        greeninputs = greeninput()
+        for x in range(5):
+            if greeninputs[x] != " ":
+                _greens[x] = greeninputs[x]
         orange = orangeinput()
-        
-        _greens = [None if x == " " else x for x in green] 
+        for x in range(5):
+            if orange[x] != " ":
+                _greyoranges[x].add(orange[x])
+        orange = orange.replace(" ", "")
         # Can't take x directly from green, overwrites _greens
         # Check for letters that are already there 
         
-        _oranges = set(list(orange)).union(_oranges) - set(list(green))
+        _oranges = set(list(orange)).union(_oranges) - set(list(greeninputs))
         our_map = defaultdict(lambda:0)
         for char in guess:
             our_map[char] += 1
-        for char in green + orange:
+        for char in greeninputs + orange:
             our_map[char] -= 1
         greys = set([x for x in our_map.keys() if our_map[x] > 0])
         _grey.update(greys)
@@ -134,8 +179,9 @@ def greeninput():
 
 
 def orangeinput(): 
-    orangetest = input("Insert all oranges, no spaces:\n")
-    if (orangetest.isalpha() and len(orangetest) <= 5) or not orangetest:
+    orangetest = input("Insert all oranges, spaces for non oranges:\n")
+    orangealphatest = orangetest.replace(" ", "")
+    if (orangealphatest.isalpha() and len(orangetest) == 5) or orangetest == "     ":
         return orangetest
     else:
         print("Wrong input, try again\n")
